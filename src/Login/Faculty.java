7d7a93bebc49f709bed3ae21db567687478e932a
Daniel <dzhu2409@gmail.com>
2022-06-21 18:35:29 -0500

    Creación de clase Faculty

--- src/Login/Faculty.java
+++ src/Login/Faculty.java
@@ -1,7 +1,62 @@
 package Login;
 
+import javax.swing.JOptionPane;
 public class Faculty {
-  void showFaculty() {
+  private String faculty;
+  
+  public String getFaculty () {
+    return faculty;
+  }
+
+  void VerifyFaculty() {
+    String faculty;
+        boolean valid = true;
+        boolean tiene_numeros = false;
+        int[] num = new int[10];
+        char[] numer = new char[10];
+        for (int i = 0; i < 10; i++) {
+            num[i] = i;
+            numer[i] = (char)(num[i] + '0');
+        }
+        do {
+            faculty = JOptionPane.showInputDialog("Ingrese la facultad");
+
+            char[] letras = new char[faculty.length()];
+            for (int i = 0; i < faculty.length(); i++) {
+                letras[i] = faculty.charAt(i);
+            }       
+            if (faculty.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Ingrese la facultad");
+                valid = false;
+                continue;
+            }
+            if (faculty.length() < 5) {
+                JOptionPane.showMessageDialog(null, "La facultad debe contener 5 o más caracteres");
+                valid = false;
+            }
+            if (letras[0] == ' ' || letras[faculty.length() - 1] == ' '){
+                JOptionPane.showMessageDialog(null, "No debe contener espacios al inicio o final");
+                valid = false;
+                continue;
+            }
+            for (int i = 0; i < faculty.length(); i++) {  
+                for (int j = 0; j < 10; j++) {
+                    if (letras[i] == numer[j]) {                   
+                        valid = false;
+                        tiene_numeros = true;
+                        continue;               
+                    }  
+                }             
+                if (letras[i] == 32 && letras[i + 1] == 32) {
+                    JOptionPane.showMessageDialog(null, "No debe escribir espacios seguidos");
+                    valid = false;
+                }   
+            }
+            if (tiene_numeros == true) {
+                JOptionPane.showMessageDialog(null, "No debe contener numeros");
+            }
+        } while (valid == false);
+        JOptionPane.showMessageDialog(null, "Facultad correcta");
 
   }
 }
