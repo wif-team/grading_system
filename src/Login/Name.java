cfbb28033a85b86ecdf14d92893edf3515bd6a2a
SrPabliss <pvillacres6317@uta.edu.ec>
2022-06-20 22:26:42 -0500

    Creado el metodo password uwu, un saludito

--- src/Login/Name.java
+++ src/Login/Name.java
@@ -6,7 +6,7 @@ public class Name {
   void showName() {
     String username = "";
     String regexUsername = "^[a-zA-Z\\s]{2,}$";
-    boolean isUsernameValid = false;
+    boolean isUsernameValid = true;
     do {
       username = JOptionPane.showInputDialog("Ingresa tu Nombre y Apellido");
 
--- src/Login/Password.java
+++ src/Login/Password.java
@@ -1,7 +1,39 @@
 package Login;
 
+import javax.swing.JOptionPane;
+
 public class Password {
   void showPassword() {
+    // Contrasena donde la unica condicion es que tenga mas de 10 caracteres (no
+    // puede contener espacios)
+    String user_password;
+    boolean validez = true;
+    do {
 
+      user_password = JOptionPane.showInputDialog(null, "Ingrese su contrasena");
+      validez = valpassword(user_password);
+
+      if (validez == false && user_password.contains(" ")) {
+        JOptionPane.showMessageDialog(null, "La contraseña no debe contener espacios y tener 10 o mas caracteres");
+
+      } else if (validez == false) {
+        JOptionPane.showMessageDialog(null, "La contrasena debe de contener 10 caracteres");
+
+      } else {
+        if (user_password.contains(" ")) {
+          JOptionPane.showMessageDialog(null, "La contraseña no debe contener espacios");
+          validez = false;
+        }
+      }
+
+    } while (validez == false);
+
+    JOptionPane.showMessageDialog(null, validez);
+
+  }
+
+  // Metodo que determina si cumple con los 10 caracteres
+  public static boolean valpassword(String user_password) {
+    return user_password.matches("^.{10,}$");
   }
 }
