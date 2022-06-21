f826995aede35ac0dcf727fe41100dddfa3bf099
Emi1213 <emi1213galarza@gmail.com>
2022-06-21 10:57:04 -0500

    Update

--- src/Login/Name.java
+++ src/Login/Name.java
@@ -1,15 +1,6 @@
 package Login;
 
-import javax.swing.JOptionPane;
-
 public class Name {
-  void showName() {
-    String username = "";
-    String regexUsername = "^[a-zA-Z\\s]{2,}$";
-    boolean isUsernameValid = false;
-    do {
-      username = JOptionPane.showInputDialog("Ingresa tu Nombre y Apellido");
-
-    } while (!isUsernameValid);
+  public void showName() {
   }
 }
