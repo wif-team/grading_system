6b09ae42619c0c6499b4ecd0a5e9dd87000789bb
Emi1213 <emi1213galarza@gmail.com>
2022-06-24 20:53:03 -0500

    Update

--- src/Login/Name.java
+++ src/Login/Name.java
@@ -8,12 +8,12 @@ public class Name {
 
   public void getNameDocent() {
     String regexUsername = "^[A-Z]{1}[a-z]{1,}[\\-\\s][A-Z]{1}[a-z]{1,}$";
-    String regexValidName = "^[A-Za-z\\s]{2,}$";
+    String regexValidName = "^[A-Za-z\\s]{1,}$";
     boolean isValidName = false;
     do {
       username = JOptionPane.showInputDialog("Ingresa tu Nombre y Apelllido");
       if (username.trim().matches(regexUsername)) {
-        JOptionPane.showMessageDialog(null, "Dato ingresado correctamente");
+        JOptionPane.showMessageDialog(null, "Dato ingresado correctamente ");
         isValidName = true;
       } else if (username.trim().isEmpty()) {
         JOptionPane.showMessageDialog(null, "El nombre no puede estar vacio");
