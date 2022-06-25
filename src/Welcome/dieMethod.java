1cbdb7df8a0167b42349900fcb6372f27898ff3c
M-anit <anamanobanda2002@gmail.com>
2022-06-24 23:09:03 -0500

    adicion de fraces

--- src/Welcome/dieMethod.java
+++ src/Welcome/dieMethod.java
@@ -25,11 +25,26 @@ public class dieMethod {
         phrases.add("Si puedes imaginarlo, puedes programarlo");
         phrases.add("No hay nada mejor que una oportunidad para ser mejor");
         phrases.add("Primero pienso luego existo");
+        phrases.add("Cree en ti y todo sera posible");
+        phrases.add("Podran robarte las ideas pero nunca el Talento");
+        phrases.add("Sere breve, Te esperan cosas hermosas solo resiste y manten la Fe");
+        phrases.add("Sin lluvia no hay flores");
+        phrases.add("Planta en ti, lo que quieres que florezca en los demas");
+        phrases.add("Confia en ti mismo, Sabes mas delo que crres que sabes");
+        phrases.add("Si tu dia esta amargo sacudelo un poco, a veces el azucar esta en el fondo");
+        phrases.add("No te detengas hasta que te sientas orgullosa/o");
+        phrases.add("Encuentra la parte positiva en todo");
+        phrases.add("La confianza en uno mismo es el primer secreto del exito");
+        phrases.add("Lo unico imposible es aquello que no lo intentas");
+        phrases.add("Deja que el --NO VAS A PODER-- de la gente sea **TU MOTIVACION");
+        
+
+
 
     }
 
     public void die() {
-        random = new Random();
+        this.random = new Random();
         this.index = random.nextInt(phrases.size());
         JOptionPane.showMessageDialog(null, phrases.get(index));
     }
